022615bd66b3d03181ad11c4b64848620f555384
aodbee <ozzaut@gmail.com>
2025-08-08 17:12:11 +0700

    fix dir path error

--- recsalmo.py
+++ recsalmo.py
@@ -1211,12 +1211,8 @@ if __name__ == '__main__':
         description='Specialized Python Program for Typing and Characterization of Salmonella Genomes',
         epilog='Thank you for using RECSALMO. Have a superb day!')
     requirement = parser.add_argument_group("requirement")
-    # requirement.add_argument("input_dir", help='Directory containing Salmonella genome assemblies')
-    # requirement.add_argument("output_dir", help='Main output directory')
-    parser.add_argument("--input_dir", default="./input", help="Directory containing Salmonella genome assemblies")
-    parser.add_argument("--output_dir", default="./output", help="Main output directory")
-
-
+    requirement.add_argument("input_dir", help='Directory containing Salmonella genome assemblies')
+    requirement.add_argument("output_dir", help='Main output directory')
     args = parser.parse_args()
     input_dir = Path(args.input_dir)
     output_dir = Path(args.output_dir)
